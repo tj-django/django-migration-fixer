301b44b913589145d4e1c46a5caf7e655d7e794e
Tonye Jack <jtonye@ymail.com>
2021-06-08 09:55:26 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     author_email="jtonye@ymail.com",
     python_requires=">=3.6",
     classifiers=[
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",

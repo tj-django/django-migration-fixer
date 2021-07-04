b87c896e8ef826805a90891549b3a26de93be3ca
Tonye Jack <jtonye@ymail.com>
2021-07-04 17:36:10 -0400

    Updated test

--- migration_fixer/tests/management/commands/utils.py
+++ migration_fixer/tests/management/commands/utils.py
@@ -17,14 +17,11 @@ def execute_command(cmd, *args, **kwargs):
 @contextlib.contextmanager
 def temporary_checkout(git_repo, target_branch_name):
     cwd = os.getcwd()
-    previous_branch_name = ""
 
     try:
         os.chdir(git_repo.workspace)
         # Clean all untracked files
         git_repo.api.git.clean("-xdf")
-
-        previous_branch_name = git_repo.api.active_branch.name
 
         target_branch = git_repo.api.heads[target_branch_name]
 
@@ -33,9 +30,6 @@ def temporary_checkout(git_repo, target_branch_name):
         yield target_branch
 
     finally:
-        if previous_branch_name:
-            git_repo.api.heads[previous_branch_name].checkout(force=True)
-
         # Clean all untracked files
         git_repo.api.git.clean("-xdf")
 

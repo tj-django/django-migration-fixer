43e4e2a12e738a3b9e7c2f96ab7ab876bbe1f1c8
Tonye Jack <jtonye@ymail.com>
2021-07-09 12:57:14 -0400

    Update constants.py

--- migration_fixer/tests/management/commands/constants.py
+++ migration_fixer/tests/management/commands/constants.py
@@ -1,5 +1,9 @@
 DEFAULT_BRANCH = "main"
-TEST_01_MIGRATION_BRANCH = "feature/migration-test-01"  # Single merge to main
-TEST_02_MIGRATION_BRANCH = "feature/migration-test-02"  # Single merge to main after 01 is merged (single conflict)
-TEST_03_MIGRATION_BRANCH = "feature/migration-test-03"  # Single merge to main after 01 is merged (multiple conflicts)
-TEST_04_MIGRATION_BRANCH = "feature/migration-test-04"  # Single merge to main after 01 is merged (named module)
+# Single merge to main
+TEST_01_MIGRATION_BRANCH = "feature/migration-test-01"
+# Single merge to main after 01 is merged (single conflict)
+TEST_02_MIGRATION_BRANCH = "feature/migration-test-02"
+# Single merge to main after 01 is merged (multiple conflicts)
+TEST_03_MIGRATION_BRANCH = "feature/migration-test-03"
+# Single merge to main after 01 is merged (named module)
+TEST_04_MIGRATION_BRANCH = "feature/migration-test-04"
